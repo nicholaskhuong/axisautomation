b383b95ad57417599268cfda86bd80dc8d7e1b2e
NicholasKHUONG <nicholas@enclave.vn>
2017-02-28 14:53:18 +0700

    Nghia Khuong fix login

--- AxisAutomation/src/com/abb/ventyx/axis/AsixLogin.java
+++ AxisAutomation/src/com/abb/ventyx/axis/AsixLogin.java
@@ -17,9 +17,15 @@ public class AsixLogin extends BaseTestCase {
 	    driver.findElement(By.id("gwt-uid-7")).sendKeys("1094");
 	    driver.findElement(By.id("gwt-uid-9")).clear();
 	    driver.findElement(By.id("gwt-uid-9")).sendKeys("Testuser1");
-	    driver.findElement(By.id("gwt-uid-22")).clear();
-	    driver.findElement(By.xpath("//span[@text='Login']")).click();
+//	    driver.findElement(By.xpath("//span[@text='v-button-caption']/select")).click();
+	    WebElement loginButon = driver.findElement(By.cssSelector("div.v-horizontallayout > div.v-expand > div.v-slot > div.v-button-primary > span.v-button-wrap > span.v-button-caption"));
+	    JavascriptExecutor js = (JavascriptExecutor)driver;
+	    js.executeScript("arguments[0].click();", loginButon);
 	    
+	    WebElement continueButton = driver.findElement(By.cssSelector("div.v-horizontallayout > div.v-expand > div.v-align-right > div.v-widget > span.v-button-wrap > span.v-button-caption"));
+	    js.executeScript("arguments[0].click();", continueButton);
+	    
+	    driver.findElement(By.id("gwt-uid-22")).clear();
 	    driver.findElement(By.id("gwt-uid-22")).sendKeys("Test 1");
   }
 }
