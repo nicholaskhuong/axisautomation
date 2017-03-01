ae7803671cc9ec39dade75ef8d8724fe94a8400e
NicholasKHUONG <nicholas@enclave.vn>
2017-03-01 10:25:10 +0700

    Update to fixed ID

--- AxisAutomation/src/com/abb/ventyx/axis/AxisLogin.java
+++ AxisAutomation/src/com/abb/ventyx/axis/AxisLogin.java
@@ -15,16 +15,15 @@ public class AxisLogin extends BaseTestCase {
   public void Login() throws Exception {
 //	  	WebDriver driver= super.driver;
 	  	driver.get(Constants.HOME_URL + "/SupplierPortal/#!listSupplier");
-	    driver.findElement(By.id("gwt-uid-7")).clear();
-	    driver.findElement(By.id("gwt-uid-7")).sendKeys("1094");
-	    driver.findElement(By.id("gwt-uid-9")).clear();
-	    driver.findElement(By.id("gwt-uid-9")).sendKeys("Testuser1");
-//	    driver.findElement(By.xpath("//span[@text='v-button-caption']")).click();
-	    WebElement loginButon = driver.findElement(By.cssSelector("div.v-horizontallayout > div.v-expand > div.v-slot > div.v-button-primary > span.v-button-wrap > span.v-button-caption"));
+	  	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    driver.findElement(By.id("userSeqNo")).clear();
+	    driver.findElement(By.id("userSeqNo")).sendKeys("1094");
+	    driver.findElement(By.id("password")).click();
+	    driver.findElement(By.cssSelector("input[type='password']")).sendKeys("Testuser1");
+	    driver.findElement(By.xpath("//div[@id='signin']")).click();
 	    JavascriptExecutor js = (JavascriptExecutor)driver;
-	    js.executeScript("arguments[0].click();", loginButon);
+	    WebElement continueButton = driver.findElement(By.cssSelector("div.v-horizontallayout > div.v-expand > div.v-align-right > div.v-widget "));
 	    
-	    WebElement continueButton = driver.findElement(By.cssSelector("div.v-horizontallayout > div.v-expand > div.v-align-right > div.v-widget > span.v-button-wrap > span.v-button-caption"));
 	    js.executeScript("arguments[0].click();", continueButton);
 	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 //	    driver.findElement(By.id("gwt-uid-22")).clear();
