ee5588f186419dcb823652dad1d99c1c174579a3
NicholasKHUONG <nicholas@enclave.vn>
2017-03-08 16:04:34 +0700

    Nghia Khuong change dirver

--- AxisP2PAutomation/src/com/abb/ventyx/axis/customer/SearchTC.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/customer/SearchTC.java
@@ -9,7 +9,6 @@ import com.abb.ventyx.utilities.BaseTestCase;
 public class SearchTC extends BaseTestCase {
 	@Test
 	public void searchGoogle() {
-	WebDriver driver= super.driver;
 	driver.get("http://google.com.vn" + "/");
     driver.findElement(By.id("lst-ib")).clear();
     driver.findElement(By.id("lst-ib")).sendKeys("khuong dai nghia");
