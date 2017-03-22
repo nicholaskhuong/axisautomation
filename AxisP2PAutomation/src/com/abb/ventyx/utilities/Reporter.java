961fdb4939e10aa6b2f26c9d45abb7ca38e66fee
Kayo <khoi.tran>
2017-03-22 13:51:49 +0700

    Kayo's fixing 1.

--- AxisP2PAutomation/src/com/abb/ventyx/utilities/Reporter.java
+++ AxisP2PAutomation/src/com/abb/ventyx/utilities/Reporter.java
@@ -42,7 +42,7 @@ public class Reporter implements IReporter {
 		// Iterating over each suite included in the test
 		// System.out.println(new Gson().toJson(allResults));
 
-		// System.out.println(new Gson().toJson(allResults));
+		System.out.println("TK ALL RESULT = " + new Gson().toJson(allResults));
 //		List<ITestResult> allTestResults;
 
 		createTestPackageAdapter();
@@ -53,7 +53,7 @@ public class Reporter implements IReporter {
 
 		//System.out.println(new Gson().toJson(allPackages));
 
-		System.out.println(new Gson().toJson("TK ALL RESULT = " + new Gson().toJson(allResults)));
+		
 
 		generateTheFinalReport();
 
