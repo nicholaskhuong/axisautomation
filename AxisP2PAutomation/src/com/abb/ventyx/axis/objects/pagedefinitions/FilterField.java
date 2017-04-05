f833d773745fa84199112f087ec21ef769a45c92
Tam (Tywin) T. DO <tywin@enclave.vn>
2017-04-05 15:01:58 +0700

    Tywin added new App

---
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/FilterField.java
@@ -0,0 +1,27 @@
+package com.abb.ventyx.axis.objects.pagedefinitions;
+
+public class FilterField {
+	public static final String ADD = "#HeaderMenuBar > span > span > span";
+ 	public static final String FIROW1 = "docTypeBtn0";
+ 	public static final String FIROW2 = "docTypeBtn1";
+ 	public static final String FIROW3 = "docTypeBtn2";
+ 	public static final String DELROW1 = "deleteItemBtn0";
+ 	public static final String DELROW2 = "deleteItemBtn1";
+ 	public static final String DELROW3 = "deleteItemBtn2";
+ 	public static final String DTROW1 = "#content-component > div > div.v-panel-content.v-panel-content-borderless.v-panel-content-v-common-page-panel.v-scrollable > div > div.v-slot.v-slot-v-common-page-content-layout > div > div > div > div > div > div > div.v-grid-tablewrapper > table > tbody > tr:nth-child(1) > td:nth-child(2)";
+ 	public static final String FDROW1 = "#content-component > div > div.v-panel-content.v-panel-content-borderless.v-panel-content-v-common-page-panel.v-scrollable > div > div.v-slot.v-slot-v-common-page-content-layout > div > div > div > div > div > div > div.v-grid-tablewrapper > table > tbody > tr:nth-child(1) > td:nth-child(3)";
+ 	public static final String LTROW1 = "#content-component > div > div.v-panel-content.v-panel-content-borderless.v-panel-content-v-common-page-panel.v-scrollable > div > div.v-slot.v-slot-v-common-page-content-layout > div > div > div > div > div > div > div.v-grid-tablewrapper > table > tbody > tr:nth-child(1) > td:nth-child(4)";
+ 	public static final String LROW1 = "#content-component > div > div.v-panel-content.v-panel-content-borderless.v-panel-content-v-common-page-panel.v-scrollable > div > div.v-slot.v-slot-v-common-page-content-layout > div > div > div > div > div > div > div.v-grid-tablewrapper > table > tbody > tr:nth-child(1) > td:nth-child(5)";
+ 	public static final String FTROW1 = "#content-component > div > div.v-panel-content.v-panel-content-borderless.v-panel-content-v-common-page-panel.v-scrollable > div > div.v-slot.v-slot-v-common-page-content-layout > div > div > div > div > div > div > div.v-grid-tablewrapper > table > tbody > tr:nth-child(1) > td:nth-child(6)";
+ 	public static final String DOC_TYPE = "docTypeCB";
+ 	public static final String FIELD_NAME = "fieldName";
+ 	public static final String COLUMN_NAME = "locationColumnNameCB";
+ 	public static final String FIELD_TYPE = "fieldTypeCB";
+ 	public static final String LOCATION_TYPE = "locationTypeCB";
+ 	public static final String TABLE_NAME = "locationTableNameCB";
+ 	public static final String ADD_CACEL = "cancelBtn";
+ 	public static final String ADD_SAVE = "createEditBtn";
+ 	public static final String CONFIRM_MESSAGE = "#confirmation-window > div > div > div.v-window-contents > div > div > div.v-slot.v-slot-v-confirmation-window-text-and-button-layout > div > div:nth-child(1) > div";
+ 	public static final String NO = "noBtn";
+ 	public static final String YES = "okBtn";
+}
