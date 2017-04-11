f23240c9b4f357f0bbd9f39a9718848119205592
Datherine <datherine@enclave.vn>
2017-04-11 17:25:19 +0700

    Datherine add TC 162

---
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/AxisSupportUserGroup.java
@@ -0,0 +1,19 @@
+package com.abb.ventyx.axis.objects.pagedefinitions;
+
+public class AxisSupportUserGroup {
+	public static final String CONFIGURATION_MENU_CSS= "#axisConfigParentButton > span > span.valo-menu-item-caption > div > span.v-menu-item-caption";
+	public static final String USERGROUP_SUBMENU_CSS= "#maintainUsergroupButton > span > span";
+	public static final String ADD_CSS= "#HeaderMenuBar > span:nth-child(1) > span > span";
+	public static final String USERGROUP_NAME_ID= "userGroupName";
+	public static final String PERMISSION_GRID_XPATH= "//*[@id='permissionGrid']";
+	public static final String PERMISSION_GRID_ID= "permissionGrid";
+	public static final String PERMISSION_GRID_ROW_XPATH= "//*[@id='permissionGrid']/div[3]/table/tbody/tr[";
+	public static final String PERMISSION_GRID_DOCTYPEHEADER_XPATH= "//*[@id='permissionGrid']/div[3]/table/thead/tr/th[2]/div[1]";
+	public static final String PERMISSION_CHECKBOXALL_CLASS = "v-grid-select-all-checkbox";
+	public static final String PERMISSION_CHECKBOX_CLASS = "v-grid-selection-checkbox";
+	public static final String SCROLL_BAR_XPATH="//*[@id='permissionGrid']/div[1]";
+	public static final String Save_ID= "saveBtn";
+	public static final String CONFIRM_DAILOGUE_CSS = "#SupplierPortal-1227837064-overlays > div.v-Notification.v-error-notification.v-Notification-v-error-notification";
+	public static final String CONFIRM_MESSAGE_CSS = "#SupplierPortal-1227837064-overlays > div.v-Notification.v-error-notification.v-Notification-v-error-notification > div.popupContent > div.gwt-HTML h1.v-Notification-caption > div.v-horizontallayout.v-layout.v-horizontal.v-widget.v-has-width > div.v-expand > div.v-slot.v-align-center.v-align-middle.v-notification-inline-caption";
+	public static final String USERGROUP_GRID_XPATH="//*[@id='content-component']";
+}
