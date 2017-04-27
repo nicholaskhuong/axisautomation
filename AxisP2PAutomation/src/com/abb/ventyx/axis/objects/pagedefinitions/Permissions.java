9bbc9ecc6d52b11144f77f1c9b652c558cc2d1c1
Tam (Tywin) T. DO <tywin@enclave.vn>
2017-04-27 17:28:17 +0700

    Tywin updated script 155

--- AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/Permissions.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/Permissions.java
@@ -1,8 +1,8 @@
 package com.abb.ventyx.axis.objects.pagedefinitions;
 
 public class Permissions {
-		
-	 	public static final String ADD = "#HeaderMenuBar > span > span > span";
+		public static final String GRID = "#content-component > div > div.v-panel-content.v-panel-content-borderless.v-panel-content-v-common-page-panel.v-scrollable > div > div > div.v-slot.v-slot-v-common-page-content-layout > div > div > div > div > div > div > div > div.v-grid-tablewrapper > table";
+	 	public static final String ADD = "#HeaderMenuBar > span:nth-child(2) > span > span";
 	 	public static final String PERMISSION_ID_FILTER = "(//input[@id='filterField'])[1]";
 	 	public static final String DOCUMENT_TYPE = "doc_type";
 	 	public static final String DOCUMENT_TYPE_FILTER = "(//input[@id='filterField'])[2]";
@@ -18,7 +18,7 @@ public class Permissions {
 	 	public static final String TEST_DOCUMENT_TYPE = "#VAADIN_COMBOBOX_OPTIONLIST > div > div.v-filterselect-suggestmenu > table > tbody > tr:nth-child(7) > td > span";
 	 	public static final String TESTING = "#VAADIN_COMBOBOX_OPTIONLIST > div > div.v-filterselect-suggestmenu > table > tbody > tr:nth-child(8) > td > span";
 	 	public static final String SAVE = "createEditBtn";
-	 	public static final String CANCEL = "cancelBtn";
+	 	public static final String CANCEL = "cancelBtn"; 
 	 	public static final String CONFIRMATION_OF_DELETION = "#confirmation-window > div > div > div.v-window-contents > div > div > div.v-slot.v-slot-v-confirmation-window-text-and-button-layout > div > div:nth-child(1) > div";
 	 	public static final String DELETE_YES = "#confirmation-window > div > div > div.v-window-contents > div > div > div.v-slot.v-slot-v-confirmation-window-text-and-button-layout > div > div.v-slot.v-slot-v-bottombar-button-layout.v-align-right.v-align-middle > div > div:nth-child(3) > div";
 	 	public static final String DELETE_NO = "#confirmation-window > div > div > div.v-window-contents > div > div > div.v-slot.v-slot-v-confirmation-window-text-and-button-layout > div > div.v-slot.v-slot-v-bottombar-button-layout.v-align-right.v-align-middle > div > div:nth-child(1) > div";
@@ -42,5 +42,6 @@ public class Permissions {
 	 	public static final String DELETE1 = "deleteItemBtn1";
 	 	public static final String DELETE2 = "deleteItemBtn2";
 	 	public static final String DELETE45 = "deleteItemBtn45";
+	 	public static final String POPUP_SUCCESS = "#SupplierPortal-1227837064-overlays > div.v-Notification.v-success-notification.v-Notification-v-success-notification";
 	 		 	
 }
--- AxisP2PAutomation/src/com/abb/ventyx/axis/support/Permissions_Updating.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/support/Permissions_Updating.java
@@ -7,62 +7,60 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.Assert.*;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.abb.ventyx.utilities.ALM;
+import com.abb.ventyx.utilities.BaseGrid;
 import com.abb.ventyx.utilities.BaseTestCase;
 import com.abb.ventyx.utilities.Credentials;
 import com.abb.ventyx.axis.objects.pagedefinitions.AxisConfigMenu;
+import com.abb.ventyx.axis.objects.pagedefinitions.Messages;
 import com.abb.ventyx.axis.objects.pagedefinitions.Permissions;
 import com.ventyx.testng.TestDataKey;
 
 @ALM(id = "155") 
 @Credentials(user = "5", password = "testuser")
 public class Permissions_Updating extends BaseTestCase {
-	
-	@TestDataKey private final String PERMISSION_NAME_A = "Export PO ABB";
-	@TestDataKey private final String PERMISSION_NAME_B = "AA_MAINTAIN_PERMISSION";
-	@TestDataKey private final String DOCUMENT_TYPE_A = "POAck";
-	@TestDataKey private final String DOCUMENT_TYPE_B = "SYSTEM";
-	@TestDataKey private final String USER_TYPE_A = "CSA";
-	@TestDataKey private final String USER_TYPE_B = "A";
-	
+	@TestDataKey private final String PERMISSION_NAME_A = "AA_MAINTAIN_PERMISSION";
+	@TestDataKey private final String PERMISSION_NAME_B = "Export PO ABB";
+	BaseGrid grid;
+	int row;
 	@Test
-	  public void login() throws Exception {
-		    WebElement axisConfigParentButton = (new WebDriverWait(driver, 10))
+	  public void updatePermission() throws Exception {
+		   // Create Permission 
+		    WebElement axisConfigParentButton = (new WebDriverWait(driver, 60))
 		  			.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(AxisConfigMenu.AXIS_CONFIGURATION)));
 		    axisConfigParentButton.click();
-		 // Choose one Permission to be updated, assert all the given details of Permission. 		    
-		    driver.findElement(By.cssSelector(AxisConfigMenu.PERMISSIONS)).click();
-		    driver.findElement(By.xpath(Permissions.PERMISSION_NAME_FILTER)).clear();
-		    driver.findElement(By.xpath(Permissions.PERMISSION_NAME_FILTER)).sendKeys(PERMISSION_NAME_A);
-		    assertEquals(driver.findElement(By.cssSelector(Permissions.PNROW1)).getText(),PERMISSION_NAME_A);
-		    assertEquals(driver.findElement(By.cssSelector(Permissions.UTROW1)).getText(),USER_TYPE_A);
-		    assertEquals(driver.findElement(By.cssSelector(Permissions.DTROW1)).getText(),DOCUMENT_TYPE_A);
-		 
-		 //Update the Permission with Document Type and User Type.
-		    driver.findElement(By.id(Permissions.ROW0)).click();
-		    driver.findElement(By.id(Permissions.DOCUMENT_TYPE)).click();
-		    driver.findElement(By.cssSelector(Permissions.SYSTEM)).click();
-		    driver.findElement(By.cssSelector(Permissions. CUSTOMER)).click();
-		    driver.findElement(By.cssSelector(Permissions. SUPPLIER)).click();
+		    WebElement axisConfigPermission = (new WebDriverWait(driver, 60))
+		  			.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(AxisConfigMenu.PERMISSIONS)));
+		    axisConfigPermission.click();
+		    grid = new BaseGrid(driver, Permissions.GRID);
+		    row = grid.findItemByColumnName("Permission Name", PERMISSION_NAME_B);
+		    driver.findElement(By.id("permissionIdBtn" + (row-1))).click();
+		    WebElement permissionName = driver.findElement(By.id(Permissions.PERMISSION_NAME));
+		    permissionName.clear();
+		    permissionName.sendKeys(PERMISSION_NAME_A);
 		    driver.findElement(By.id(Permissions.SAVE)).click();
-		 
-		 //Determine the updated details on the table.
-		    driver.findElement(By.xpath(Permissions.PERMISSION_NAME_FILTER)).clear();
-		    driver.findElement(By.xpath(Permissions.PERMISSION_NAME_FILTER)).sendKeys(PERMISSION_NAME_A);
-		    assertEquals(driver.findElement(By.cssSelector(Permissions.PNROW1)).getText(),PERMISSION_NAME_A);
-		    assertEquals(driver.findElement(By.cssSelector(Permissions.UTROW1)).getText(),USER_TYPE_B);
-		    assertEquals(driver.findElement(By.cssSelector(Permissions.DTROW1)).getText(),DOCUMENT_TYPE_B);
-		 
-		 //Return the updated details of Permission to its original value for the next time of test.
-		    driver.findElement(By.id(Permissions.ROW0)).click();
-		    driver.findElement(By.id(Permissions.DOCUMENT_TYPE)).click();
-		    driver.findElement(By.cssSelector(Permissions.PURCHASE_ORDERS)).click();
-		    driver.findElement(By.cssSelector(Permissions. CUSTOMER)).click();
-		    driver.findElement(By.cssSelector(Permissions. SUPPLIER)).click();
-		    driver.findElement(By.id(Permissions.SAVE)).click();
+		    WebElement flashMessage = (new WebDriverWait(driver, 60))
+		  			.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Permissions.POPUP_SUCCESS)));
+		    Assert.assertEquals(driver.findElement(By.cssSelector(Permissions.POPUP_SUCCESS)).getCssValue("background-color"), "rgba(33, 190, 137, 1)");
+		    Assert.assertEquals(flashMessage.getCssValue("visibility"), "visible");
+		    Assert.assertEquals(flashMessage.getCssValue("display"), "block");
+		    Assert.assertEquals(flashMessage.getText(), Messages.PERMISSION_UPADTED_SUCCESSFULLY);
 		    
+	}
+	   @Test(dependsOnMethods = "updatePermission")
+			public void returnPermissionToOriginalValue (){
+		    grid = new BaseGrid(driver, Permissions.GRID); 
+		    row = grid.findItemByColumnName("Permission Name", PERMISSION_NAME_A);
+		    Assert.assertNotEquals(row, -1, "Record not found");
+		    driver.findElement(By.id("permissionIdBtn" + (row-1))).click();
+		    WebElement permissionName = (new WebDriverWait(driver, 60))
+		  			.until(ExpectedConditions.presenceOfElementLocated(By.id(Permissions.PERMISSION_NAME)));
+		    permissionName.clear();
+		    permissionName.sendKeys(PERMISSION_NAME_B);
+		    driver.findElement(By.id(Permissions.SAVE)).click();
 	}	    
 }
 
