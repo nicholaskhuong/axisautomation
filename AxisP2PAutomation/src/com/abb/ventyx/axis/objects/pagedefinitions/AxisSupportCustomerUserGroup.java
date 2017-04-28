70b4c06d993b61d39c186f0d9c91189be2740142
Datherine <datherine@enclave.vn>
2017-04-28 17:06:43 +0700

    Dathy add new UserGroup file

---
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/AxisSupportCustomerUserGroup.java
@@ -0,0 +1,20 @@
+package com.abb.ventyx.axis.objects.pagedefinitions;
+
+public class AxisSupportCustomerUserGroup {
+	public static final String CUSTOMERMAINTAINCE_MENU_ID= "custMaintParentButton";
+	public static final String USERGROUP_SUBMENU_ID= "maintainAxisCustomerUsergroupButton";
+	public static final String SCREEN_TITLE_ID= "common-header-left-layout";
+	public static final String SYSTEM_TAB_ID= "systemGridTab";
+	public static final String USER_TAB_ID= "userGridTab";
+	public static final String USER_CUSTOMER_ID= "custCombo";
+	public static final String ROW0_ID= "userGroupNameBtn0";
+	public static final String ADD_XPATH= "//*[@id='content-component']/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div/div/div[3]/div/div/div/div/div/div[1]/div/div/div/div/span[1]";
+	public static final String SCREEN_TITLE="Maintain User Groups";
+	public static final String SCREEN_CREATE_TITLE="Create User Group";
+	public static final String USERGROUP_NAME_ID="userGroupName";
+	public static final String PERMISSION_CSS="userGroupName";
+	public static final String SAVE_ID= "saveBtn";
+	public static final String CANCEL_ID= "cancelBtn";
+	public static final String LIST = "//*[@id='VAADIN_COMBOBOX_OPTIONLIST']/div";
+
+}
