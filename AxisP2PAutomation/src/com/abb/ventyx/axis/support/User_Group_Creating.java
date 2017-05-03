b62254fcff8a9cb027721ea7d26c671135f03372
Datherine <datherine@enclave.vn>
2017-05-03 15:11:29 +0700

    Dathy ipdate User group

--- AxisP2PAutomation/src/com/abb/ventyx/axis/support/User_Group_Creating.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/support/User_Group_Creating.java
@@ -5,17 +5,17 @@ import static org.testng.Assert.assertEquals;
 import java.util.List;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.abb.ventyx.axis.objects.pagedefinitions.AxisSupportCustomerUserGroup;
 import com.abb.ventyx.axis.objects.pagedefinitions.Messages;
 import com.abb.ventyx.utilities.ALM;
-import com.abb.ventyx.utilities.BaseDropDownList;
+
 import com.abb.ventyx.utilities.BaseTestCase;
 import com.abb.ventyx.utilities.Credentials;
-import com.abb.ventyx.utilities.DropDownInputMethod;
+
 
 import org.testng.annotations.Test;
 
