cf068d142d1107c67eceb6200fd420cabbfd3e3b
Datherine <datherine@enclave.vn>
2017-09-12 09:05:10 +0700

    Dathy rename file

---
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/ScreenObjects.java
@@ -0,0 +1,23 @@
+package com.abb.ventyx.axis.objects.pagedefinitions;
+
+public class ScreenObjects {
+	public static final String ADD_BTN_CSS = "#HeaderMenuBar > span:nth-child(2)";
+	public static final String FILTER_BTN_CSS = "#HeaderMenuBar > span:nth-child(1)";
+	public static final String YES_BTN_ID = "okBtn";
+	public static final String NO_BTN_ID = "noBtn";
+	public static final String SAVE_ID = "saveBtn";
+	public static final String CANCEL_ID = "cancelBtn";
+	public static final String SCREEN_TITLE_ID = "common-header-left-layout";
+	public static final String FILTER_FIELD_ID = "filterField";
+	public static final String DELETE_YES = "#confirmation-window > div > div > div.v-window-contents > div > div > div.v-slot.v-slot-v-confirmation-window-text-and-button-layout > div > div.v-slot.v-slot-v-bottombar-button-layout.v-align-right.v-align-middle > div > div:nth-child(3) > div";
+	public static final String DELETE_NO = "#confirmation-window > div > div > div.v-window-contents > div > div > div.v-slot.v-slot-v-confirmation-window-text-and-button-layout > div > div.v-slot.v-slot-v-bottombar-button-layout.v-align-right.v-align-middle > div > div:nth-child(1) > div";
+	public static final String CONFIRMATION = "#confirmation-window > div > div > div.v-window-contents > div > div > div.v-slot.v-slot-v-confirmation-window-text-and-button-layout > div > div:nth-child(1) > div";
+	public static final String CONFIRM_MESSAGE = "#confirmation-window > div > div > div.v-window-contents > div > div > div.v-slot.v-slot-v-confirmation-window-text-and-button-layout > div";
+	public static final String SUCCESS_MESSAGE = "#SupplierPortal-1227837064-overlays > div.v-Notification.v-success-notification.v-Notification-v-success-notification > div > div > h1 > div > div > div.v-slot.v-align-center.v-align-middle.v-notification-inline-caption";
+	public static final String POPUP_SUCCESS = "#SupplierPortal-1227837064-overlays > div.v-Notification.v-success-notification.v-Notification-v-success-notification";
+	public static final String ERROR_CSS = "#SupplierPortal-1227837064-overlays > div.v-Notification.v-error-notification.v-Notification-v-error-notification > div > div > h1 > div > div > div.v-slot.v-align-center.v-align-middle.v-notification-inline-caption";
+	public static final String ERROR_WITHOUT_ICON_CSS = "#SupplierPortal-1227837064-overlays > div.v-Notification.v-error-notification-no-icon.v-Notification-v-error-notification-no-icon";
+	public static final String WARNING_MESSAGE_XPATH = "//*[@id='SupplierPortal-1227837064-overlays']/div[2]/div/div/h1/div/div/div[2]";
+	public static final String UNSAVED_CHANGE_CSS = "#confirmation-window > div > div > div.v-window-contents > div > div > div.v-slot.v-slot-v-confirmation-window-text-and-button-layout > div > div:nth-child(1) > div";
+	public static final String EXPAND_CLASS = "v-icon material-icons";
+}
