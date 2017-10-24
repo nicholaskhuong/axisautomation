de5a55bd310662cf41b6b24309df70e6f7588bca
NicholasKHUONG <nicholas@enclave.vn>
2017-10-24 09:55:21 +0700

    Update some neccessary code

--- AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/HomePageDefinition.java
+++
@@ -1,9 +0,0 @@
-package com.abb.ventyx.axis.objects.pagedefinitions;
-
-public class HomePageDefinition {
-	public static final String EMAIL_TEXTFIELD_ID = "email";
-	public static final String LOGIN_BUTTON_ID = "loginButton";
-	public static final String VCODE_TEXTFIELD_ID = "vCode";
-	public static final String VERIFY_BUTTON_ID = "verifyButton";
-	
-}
