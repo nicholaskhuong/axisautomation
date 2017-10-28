11091fa6c4f0f8fd7a177250606b539cd77b5c13
Phuong (Perla) T. TRINH <perla@enclave.vn>
2017-10-28 12:00:54 +0700

    Fixed fail scripts TC612

--- AxisP2PAutomation/src/com/abb/ventyx/axis/customer/CustomerUser_Updated_ByAdmin.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/customer/CustomerUser_Updated_ByAdmin.java
@@ -3,9 +3,6 @@ package com.abb.ventyx.axis.customer;
 import static org.testng.Assert.assertEquals;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import com.abb.ventyx.axis.objects.pagedefinitions.CustomerMenu;
@@ -23,7 +20,6 @@ import com.abb.ventyx.utilities.TableFunction;
 public class CustomerUser_Updated_ByAdmin extends BaseTestCase {
 	ScreenAction action;
 	TableFunction table;
-	WebDriverWait wait;
 	String CUSTOMERUSEREMAIL = "cuser1@abb.com";
 	String INVALIDCUSTOMERUSEREMAIL = "cuser1abb.com";
 	String INVALIDPASSWORD = "<html>";
@@ -35,143 +31,137 @@ public class CustomerUser_Updated_ByAdmin extends BaseTestCase {
 	String ACTIONSTATUS ="Active";
 	String CREATEDSTATUS ="Created";
 	int i;
+
+	// String userNo = " ";
 	// Step 1 Select Users Sub Menu
 	@Test
 	public void selectUsersSubMenu() {
-		WebDriverWait wait = new WebDriverWait(driver, 60);
-		WebElement customerMaintenanceMenu = (new WebDriverWait(driver, 120))
-				.until(ExpectedConditions.presenceOfElementLocated(By
-						.cssSelector(CustomerMenu.CUSTOMERMAINTENANCE_MENU)));
-		customerMaintenanceMenu.click();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By
-				.cssSelector(CustomerMenu.USERS_SUBMENU)));
-		driver.findElement(By.cssSelector(CustomerMenu.USERS_SUBMENU)).click();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By
-				.cssSelector(CustomerUsers.CUSTOMERUSERS_HEADER)));
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(CustomerUsers.ADD_BUTTON)));
-		// Thread.sleep(2000);
-		assertEquals(driver.findElement(By.cssSelector(CustomerUsers.CUSTOMERUSERS_HEADER)).getText(), "Maintain Customer Users");
+		table = new TableFunction(driver);
+		action = new ScreenAction(driver);
+
+		action.waitObjVisibleAndClick(By.cssSelector(CustomerMenu.CUSTOMERMAINTENANCE_MENU));
+		action.waitObjVisibleAndClick(By.cssSelector(CustomerMenu.USERS_SUBMENU));
+		action.waitObjVisible(By.cssSelector(CustomerUsers.ADD_BUTTON));
+		action.assertTitleScreen("Maintain Customer Users");
 	}
 
 	// Step 2 Click on User Number of an existing customer user
 	@Test(dependsOnMethods="selectUsersSubMenu")
 	public void openModifyUserScreen() {
-		table = new TableFunction(driver);
-		i = table.findRowByString(3,CUSTOMERUSEREMAIL);
-		// Thread.sleep(1000);
-		assertEquals(table.getValueRow(2,i), USERID);
-		assertEquals(table.getValueRow(4,i), "All Permissions");
-		assertEquals(table.getValueRow(5,i), CREATEDSTATUS);
+		action.pause(1000);
+		i = table.findRowByString(3, CUSTOMERUSEREMAIL);
+		System.out.print("Row: " + i + " ");
+		assertEquals(table.getValueRow(2, i), USERID);
+		assertEquals(table.getValueRow(4, i), "All Permissions");
+		assertEquals(table.getValueRow(5, i), CREATEDSTATUS);
+		// userNo = table.getValueRow(1, i);
 		table.clickUserNumber(CUSTOMERUSEREMAIL);
+
 	}
 
 	// Step 3 Update with Invalid Email
 	@Test(dependsOnMethods="openModifyUserScreen")
 	public void updateWithInvalidEmail() {
-		wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(CustomerUsers.USEREMAILADDRESS_TEXTBOX_ID)));
-		driver.findElement(By.id(CustomerUsers.USEREMAILADDRESS_TEXTBOX_ID)).clear();
-		// Thread.sleep(500);
-		driver.findElement(By.id(CustomerUsers.USEREMAILADDRESS_TEXTBOX_ID)).sendKeys(INVALIDCUSTOMERUSEREMAIL);
-		driver.findElement(By.cssSelector(CustomerUsers.SAVE_BUTTON)).click();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS)));
-		assertEquals(driver.findElement(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS)).getText(), "Invalid email address");
+		action.waitObjVisible(By.id(CustomerUsers.USEREMAILADDRESS_TEXTBOX_ID));
+		action.inputEmailField(CustomerUsers.USEREMAILADDRESS_TEXTBOX_ID, INVALIDCUSTOMERUSEREMAIL);
+		action.waitObjVisibleAndClick(By.cssSelector(CustomerUsers.SAVE_BUTTON));
+		action.waitObjVisible(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS));
+		action.assertMessgeError(ScreenObjects.ERROR_WITHOUT_ICON_CSS, Messages.INVALID_EMAIL);
 	}
 
 	// Step 4 Update with Invalid Password.
 	@Test(dependsOnMethods="updateWithInvalidEmail")
 	public void updateWithInvalidPassword() {
 
-		driver.findElement(By.id(CustomerUsers.USEREMAILADDRESS_TEXTBOX_ID)).clear();
-		// Thread.sleep(500);
-		driver.findElement(By.id(CustomerUsers.USEREMAILADDRESS_TEXTBOX_ID)).sendKeys(CUSTOMERUSEREMAIL);
-		// Thread.sleep(500);
+		action.inputEmailField(CustomerUsers.USEREMAILADDRESS_TEXTBOX_ID, CUSTOMERUSEREMAIL);
+
 		driver.findElement(By.cssSelector(CustomerUsers.YESUPDATEPASSWORD_RADIOBUTTON)).findElement(By.tagName("label")).click();
-		// Thread.sleep(500);
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS)));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(CustomerUsers.PASSWORD_TEXTBOX_ID)));
+		action.waitObjInvisible(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS));
+		action.waitObjVisible(By.id(CustomerUsers.PASSWORD_TEXTBOX_ID));
 		driver.findElement(By.id(CustomerUsers.PASSWORD_TEXTBOX_ID)).sendKeys(INVALIDPASSWORD);
 		driver.findElement(By.id(CustomerUsers.CONFIRMPASSWORD_TEXTBOX_ID)).sendKeys(INVALIDPASSWORD);
 		driver.findElement(By.cssSelector(CustomerUsers.SAVE_BUTTON)).click();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS)));
+		action.waitObjVisible(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS));
 		assertEquals(driver.findElement(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS)).getText(),Messages.INVALID_PWD1);
 	}
 	// Step 5 Update with different confirm password and password
 	@Test(dependsOnMethods="updateWithInvalidPassword")
 	public void updateWithUnmachtedPassword() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(CustomerUsers.PASSWORD_TEXTBOX_ID)));
-		driver.findElement(By.id(CustomerUsers.PASSWORD_TEXTBOX_ID)).clear();
-		// Thread.sleep(500);
-		driver.findElement(By.id(CustomerUsers.PASSWORD_TEXTBOX_ID)).sendKeys(PASSWORD);
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS)));
+		action.waitObjVisible(By.id(CustomerUsers.PASSWORD_TEXTBOX_ID));
+
+		action.inputTextField(CustomerUsers.PASSWORD_TEXTBOX_ID, PASSWORD);
+		action.waitObjInvisible(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS));
 		driver.findElement(By.id(CustomerUsers.CONFIRMPASSWORD_TEXTBOX_ID)).clear();
-		// Thread.sleep(500);
+
 		driver.findElement(By.id(CustomerUsers.CONFIRMPASSWORD_TEXTBOX_ID)).sendKeys(CONFIRMPASSWORD);
 		driver.findElement(By.cssSelector(CustomerUsers.SAVE_BUTTON)).click();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS)));
+		action.waitObjVisible(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS));
 		assertEquals(driver.findElement(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS)).getText(),Messages.UNMATCHED_CONFIRM_PWD);
 	}
 
 	// Step 6 Update with valid data
 	@Test(dependsOnMethods="updateWithUnmachtedPassword")
 	public void updateWithValidData() {
-		driver.findElement(By.id(CustomerUsers.PASSWORD_TEXTBOX_ID)).clear();
-		// Thread.sleep(200);
-		driver.findElement(By.id(CustomerUsers.PASSWORD_TEXTBOX_ID)).sendKeys(NEWPASSWORD);
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS)));
-		driver.findElement(By.id(CustomerUsers.CONFIRMPASSWORD_TEXTBOX_ID)).clear();
-		// Thread.sleep(200);
-		driver.findElement(By.id(CustomerUsers.CONFIRMPASSWORD_TEXTBOX_ID)).sendKeys(NEWPASSWORD);
-		driver.findElement(By.id(CustomerUsers.USERID_TEXTBOX_ID)).clear();
-		// Thread.sleep(200);
-		driver.findElement(By.id(CustomerUsers.USERID_TEXTBOX_ID)).sendKeys(NEWUSERID);
+
+		action.inputTextField(CustomerUsers.PASSWORD_TEXTBOX_ID, NEWPASSWORD);
+
+		action.waitObjInvisible(By.cssSelector(ScreenObjects.ERROR_WITHOUT_ICON_CSS));
+
+		action.inputTextField(CustomerUsers.CONFIRMPASSWORD_TEXTBOX_ID, NEWPASSWORD);
+
+		action.inputTextField(CustomerUsers.USERID_TEXTBOX_ID, NEWUSERID);
 		driver.findElement(By.cssSelector(CustomerUsers.SAVE_BUTTON)).click();
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(ScreenObjects.SUCCESS_MESSAGE)));
+		action.waitObjVisible(By.cssSelector(ScreenObjects.SUCCESS_MESSAGE));
+
 		assertEquals(driver.findElement(By.cssSelector(ScreenObjects.SUCCESS_MESSAGE)).getText(), Messages.USER_UPDATE_SUCCESSFULLY);
-		assertEquals(table.getValueRow(2,i), NEWUSERID);
-		assertEquals(table.getValueRow(4,i), "All Permissions");
+		assertEquals(table.getValueRow(2, i), NEWUSERID);
+		assertEquals(table.getValueRow(4, i), "All Permissions");
+
 	}
 	// Step 7 Check Cancel button without input
 	@Test(dependsOnMethods="updateWithValidData")
 	public void checkCancelButtonWithoutInput() {
-		table = new TableFunction(driver);
-		action = new ScreenAction(driver);
 		table.clickUserNumber(CUSTOMERUSEREMAIL);
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(CustomerUsers.CANCEL_BUTTON)));
-		driver.findElement(By.cssSelector(CustomerUsers.CANCEL_BUTTON)).click();
+		action.waitObjVisible(By.id(CustomerUsers.USERID_TEXTBOX_ID));
+		action.waitObjVisible(By.cssSelector(CustomerUsers.CANCEL_BUTTON));
+		action.waitObjVisibleAndClick(By.cssSelector(CustomerUsers.CANCEL_BUTTON));
 		assertEquals(action.isElementPresent(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS)), false);
-		// Thread.sleep(1000);
-		assertEquals(driver.findElement(By.cssSelector(CustomerUsers.CUSTOMERUSERS_HEADER)).getText(), "Maintain Customer Users");
+		action.pause(1000);
+		action.waitObjVisible(By.cssSelector(CustomerUsers.ADD_BUTTON));
+		action.assertTitleScreen("Maintain Customer Users");
+
 	}
 	
 	// Step 8 Check No button on Unsaved Changes dialog 
 	@Test(dependsOnMethods="checkCancelButtonWithoutInput")
-	public void checkNoButtonOnUnsavedChangesDialog() {
+	public void checkNoButtonOnUnsavedChanges() {
+		assertEquals(action.isElementPresent(By.id("usrSequenceIdStrBtn" + i)), true);
 		table.clickUserNumber(CUSTOMERUSEREMAIL);
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.id(CustomerUsers.USERID_TEXTBOX_ID)));
-		driver.findElement(By.id(CustomerUsers.USERID_TEXTBOX_ID)).clear();
-		action.pause(200);
-		driver.findElement(By.id(CustomerUsers.USEREMAILADDRESS_TEXTBOX_ID)).sendKeys(USERID);
+		action.waitObjVisible(By.id(CustomerUsers.USERID_TEXTBOX_ID));
+		action.inputTextField(CustomerUsers.USERID_TEXTBOX_ID, "test");
 		driver.findElement(By.cssSelector(CustomerUsers.CANCEL_BUTTON)).click();
-		action.pause(2000);
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS)));
+		action.waitObjVisible(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS));
 		assertEquals(driver.findElement(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS)).getText(), Messages.UNSAVED_CHANGE);
 		driver.findElement(By.id(ScreenObjects.NO_BTN_ID)).click();
-		// Thread.sleep(200);
+		action.waitObjInvisible(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS));
 		assertEquals(action.isElementPresent(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS)), false);
-		assertEquals(driver.findElement(By.cssSelector(CustomerUsers.CUSTOMERUSERS_HEADER)).getText(), "Modify User");
+
+		action.assertTitleScreen("Modify User");
+
 	}
 
 	// Step 9 Check Cancel button on Unsaved Changes dialog
-	@Test(dependsOnMethods="checkNoButtonOnUnsavedChangesDialog")
-	public void checkYesButtonOnUnsavedChangesDialog() {
+	@Test(dependsOnMethods = "checkNoButtonOnUnsavedChanges")
+	public void checkYesButtonOnUnsavedChanges() {
 		driver.findElement(By.cssSelector(CustomerUsers.CANCEL_BUTTON)).click();
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS)));
+
+		action.waitObjVisible(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS));
 		assertEquals(driver.findElement(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS)).getText(), Messages.UNSAVED_CHANGE);
 		driver.findElement(By.id(ScreenObjects.YES_BTN_ID)).click();
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS)));
+		action.waitObjInvisible(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS));
 		assertEquals(action.isElementPresent(By.cssSelector(ScreenObjects.UNSAVED_CHANGE_CSS)), false);
-		assertEquals(driver.findElement(By.cssSelector(CustomerUsers.CUSTOMERUSERS_HEADER)).getText(), "Maintain Customer Users");
+		action.waitObjVisible(By.cssSelector(CustomerUsers.ADD_BUTTON));
+		action.assertTitleScreen("Maintain Customer Users");
 
 	}
 
