d286b7c87e39eab326b2b4f219a33030ee92a64f
Perla <perla@enclave.vn>
2017-11-01 09:51:58 +0700

    Fixed scripts Profile_Filter_Sort

--- AxisP2PAutomation/src/com/abb/ventyx/axis/support/Profile_Filter_Sort.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/support/Profile_Filter_Sort.java
@@ -65,9 +65,9 @@ public class Profile_Filter_Sort extends BaseTestCase {
 	@Test(dependsOnMethods = "clickFiterToCloseOnMaintainCustomerScreen", alwaysRun = true)
 	public void clickSortIconOnMaintainCustomerScreen(){
 		action.pause(5000);
-		assertEquals(table.getValueRow(3, 1), "Advance Shipping Notice");
+		assertEquals(table.getValueRow(1, 1), "Development Cust");
 		assertEquals(table.getValueRow(2, 1), "All Document Types");
-		assertEquals(table.getValueRow(1, 1), "Customer2");
+		assertEquals(table.getValueRow(3, 1), "Invoicing, Purchase Order Acknowledgement");
 		action.pause(waitTime);
 		action.clickBtn(By.xpath(Profiles.SORT_CUSTOMER_NAME));
 	}
@@ -76,7 +76,7 @@ public class Profile_Filter_Sort extends BaseTestCase {
 	public void checkDataOnMaintainCustomerScreen(){
 		action.pause(5000);
 		assertEquals(table.getValueRow(2, 1), "All Document Types");
-		assertEquals(table.getValueRow(3, 1), "Advance Shipping Notice, Invoicing, Purchase Orders, Purchase Order Acknowledgement, Remittance Advices");
+		assertEquals(table.getValueRow(3, 1), "Advance Shipping Notice, Invoicing, Purchase Orders");
 	}
 	
 }
