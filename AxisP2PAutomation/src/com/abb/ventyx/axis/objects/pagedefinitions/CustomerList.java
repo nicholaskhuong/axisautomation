afb5d798f96cec044a42066e4ecab4b86db46ab7
Perla <perla@enclave.vn>
2017-11-02 10:00:44 +0700

    Modify class in pagedefinitions

---
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/CustomerList.java
@@ -0,0 +1,20 @@
+package com.abb.ventyx.axis.objects.pagedefinitions;
+
+
+public class CustomerList {
+
+	//Customer
+	public static final String CUSTOMERUSERS_HEADER = "#common-header-left-layout > div > div";
+	public static final String ADD_BUTTON = "#HeaderMenuBar > span:nth-child(2)";
+	public static final String SAVE_BUTTON_ID = "saveBtn";
+	public static final String ACTIVE = "#HeaderMenuBar > span:nth-child(3) > span > span";
+	public static final String DEACTIVE = "#HeaderMenuBar > span:nth-child(4) > span > span";
+	public static final String EMAIL_FILTER = "(//input[@id='filterField'])[4]";
+	public static final String STATUS = "custStatusCB";
+	
+	//Create New Customer
+	public static final String CUSTOMER_NAME = "custNameField";
+	public static final String EMAIL_ADDRESS = "emailField";
+	public static final String SUPPLIER_LIST= "#supplierMenu > span > span";
+	public static final String TABLE_AUTHORISED_DOCUMENT_TYPES= "#content-component > div > div.v-panel-content.v-panel-content-borderless.v-panel-content-v-common-page-panel.v-scrollable > div > div.v-slot.v-slot-v-common-page-content-layout > div > div.v-slot.v-slot-v-horizontal-layout-with-border > div > div";
+}
