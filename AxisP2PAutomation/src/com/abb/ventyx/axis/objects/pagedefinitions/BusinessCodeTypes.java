b215765acfd120611788e8c45d939edafcf7ba14
Perla <perla@enclave.vn>
2017-11-09 15:34:39 +0700

    Added pagedefinitions/BusinessCodeTypes

---
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/BusinessCodeTypes.java
@@ -0,0 +1,11 @@
+package com.abb.ventyx.axis.objects.pagedefinitions;
+
+public class BusinessCodeTypes {
+	public static final String CODE_TYPE = "//*[@id='codesettypecreateeditview']/div/div/div[3]/div/div/div[2]/div/div[1]/div/div/div/div[1]/div/input";
+	public static final String CODE_DESCRIPTION = "//*[@id='codesettypecreateeditview']/div/div/div[3]/div/div/div[2]/div/div[1]/div/div/div/div[3]/div/input";
+	public static final String SAVE_BUTTON = "//*[@id='codesettypecreateeditview']/div/div/div[3]/div/div/div[2]/div/div[3]/div/div/div/div/div[3]";
+	public static final String CANCEL_BUTTON = "//*[@id='codesettypecreateeditview']/div/div/div[3]/div/div/div[2]/div/div[3]/div/div/div/div/div[1]";
+	public static final String YES = "okBtn";
+	public static final String NO = "noBtn";
+	public static final String CODE_TYPE_FILTER = "(//input[@id='filterField'])[1]";	
+}
