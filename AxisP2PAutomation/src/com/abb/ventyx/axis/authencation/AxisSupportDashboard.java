02a0ceb5bf530958fdd9603448435417b5124ab8
Phuong (Perla) T. TRINH <perla@enclave.vn>
2017-11-14 10:53:13 +0700

    Fixed compile issue

--- AxisP2PAutomation/src/com/abb/ventyx/axis/authencation/AxisSupportDashboard.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/authencation/AxisSupportDashboard.java
@@ -49,7 +49,6 @@ public class AxisSupportDashboard extends BaseTestCase {
 	String auditLog = "Audit Log";
 	String userGuide = "User Guide";
 	String about = "About";
-<<<<<<< ac8a1eaa1df4ead13a94967eeaf1c40520e5b950
 	String maintainAxisSupportUsers = "Maintain Axis Support Users";
 	String resetUserPassword = "Reset User Password";
 	String maintainUIXMLConfig = "Maintain UI XML Configuration";
@@ -60,8 +59,6 @@ public class AxisSupportDashboard extends BaseTestCase {
 	String cachePage = "Maintain Cache";
 	String axisB2BAdaptersPage = "Axis B2B Adapters";
 	String maitainDocumentTypePage = "Maintain Document Types";
-=======
->>>>>>> Rename package
 	@Test
 	public void openScreen() {
 		// Step 1
