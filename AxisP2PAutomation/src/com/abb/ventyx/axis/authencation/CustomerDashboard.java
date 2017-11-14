9ebe25c28fa126b8fce6ef59167b72ca8cb2d944
DonnaNGUYEN <donna@enclave.vn>
2017-11-14 17:17:30 +0700

    write script for TC 493

---
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/authencation/CustomerDashboard.java
@@ -0,0 +1,132 @@
+package com.abb.ventyx.axis.authencation;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import com.abb.ventyx.axis.objects.pagedefinitions.AxisConfigMenu;
+import com.abb.ventyx.axis.objects.pagedefinitions.CustomerMenu;
+import com.abb.ventyx.axis.objects.pagedefinitions.ScreenObjects;
+import com.abb.ventyx.axis.objects.pagedefinitions.UserPreferences;
+import com.abb.ventyx.utilities.ALM;
+import com.abb.ventyx.utilities.BaseTestCase;
+import com.abb.ventyx.utilities.Credentials;
+import com.abb.ventyx.utilities.ScreenAction;
+import com.abb.ventyx.utilities.TableFunction;
+
+@ALM(id = "493")
+@Credentials(user = "customer_user_1@abb.com", password = "Testuser1")
+public class CustomerDashboard extends BaseTestCase {
+	ScreenAction action;
+	TableFunction table;
+	int milliseconds = 3000;
+	String customerDashboard = "Customer Dashboard";
+	String axisSupportPortal = "Axis Supplier Portal";
+	String userName = "Customer User 1";
+	String userID = "customer_user_1@abb.com";
+	String customerMaintenance = "Customer Maintenance";
+	String notifications = "Notifications";
+	String documentation = "Documentation";
+	String supplierList = "Supplier List";
+	String userGroups = "User Groups";
+	String users = "Users";
+	String profiles = "Profiles";
+	String documentFilters = "Document Filters";
+	String busniessCodeSets = "Business Code Sets";
+	String addressContact = "Address & Contact";
+	String customiseLiterals = "Customise Literals";
+	String documentInError = "Documents in Error";
+	String auditLog = "Audit Log";
+	String configureDocument = "Configure Documentation";
+	String userGuide = "User Guide";
+	String about = "About";
+	String maintainSuppliers = "Maintain Suppliers";
+	String maitainUserGroups = "Maintain User Groups";
+	String maintainCustomerUsers = "Maintain Customer Users";
+	String maintainCustomerDefinedProfiles = "Maintain Customer Defined Profiles";
+	@Test
+	public void openScreen() {
+		// Step 1
+		action = new ScreenAction(driver);
+		action.assertTitleScreen(customerDashboard);
+	}
+	@Test(dependsOnMethods = "openScreen")
+	public void checkTopRibbon() {
+		// step 2,4
+		assertEquals(driver.findElement(By.cssSelector(AxisConfigMenu.AXIS_SUPPORT_PORTAL)).getText(), axisSupportPortal);
+		assertEquals(driver.findElement(By.id(AxisConfigMenu.LANGUAGE)).getText(), "EN");
+		assertEquals(driver.findElement(By.id(UserPreferences.PROFILE_PANEL)).getText(), userName);
+		action.clickBtn(By.id(UserPreferences.PROFILE_PANEL));
+		action.waitObjVisible(By.id(ScreenObjects.SIGNOUT_BUTTON));
+		assertEquals(driver.findElement(By.cssSelector(AxisConfigMenu.USER_LOGIN)).getText(), userName);
+		assertEquals(driver.findElement(By.cssSelector(AxisConfigMenu.USER_ID)).getText(), userID);
+		action.clickBtn(By.id(UserPreferences.PROFILE_PANEL));
+	}
+
+	@Test(dependsOnMethods = "checkTopRibbon")
+	public void checkMenuOptions() {
+		// step 3
+		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.CUSTOMER_DASHBOARD_WITHOUT_ICON)).getText(), customerDashboard);
+		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.CUSTOMER_MAINTENANCE_WITHOUT_ICON)).getText(), customerMaintenance);
+		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.NOTIFICATIONS_WITHOUT_ICON)).getText(), notifications);
+		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.DOCUMENTATION_WITHOUT_ICON)).getText(), documentation);
+	}
+
+	@Test(dependsOnMethods = "checkMenuOptions")
+	public void checkCustomerMaintenance() {
+		// step 5,6
+		action.clickBtn(By.id(CustomerMenu.CUSTOMER_MAINTENANCE));
+		action.waitObjVisible(By.cssSelector(CustomerMenu.SUPPLIER_LIST));
+		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.SUPPLIER_LIST)).getText(), supplierList);
+		assertEquals(driver.findElement(By.id(CustomerMenu.USER_GROUPS)).getText(), userGroups);
+		assertEquals(driver.findElement(By.id(CustomerMenu.USERS)).getText(), users);
+		assertEquals(driver.findElement(By.id(CustomerMenu.PROFLES)).getText(), profiles);
+		assertEquals(driver.findElement(By.id(CustomerMenu.DOCUMENT_FILTERS)).getText(), documentFilters);
+		assertEquals(driver.findElement(By.id(CustomerMenu.BUSINESS_CODE_SET)).getText(), busniessCodeSets);
+		assertEquals(driver.findElement(By.id(CustomerMenu.ADDRESS_CONTACT)).getText(), addressContact);
+		assertEquals(driver.findElement(By.id(CustomerMenu.CUSTOMISE_LITERALS)).getText(), customiseLiterals);
+		assertEquals(driver.findElement(By.id(CustomerMenu.DOCUMENT_IN_ERROR)).getText(), documentInError);
+		assertEquals(driver.findElement(By.id(CustomerMenu.AUDIT_LOG)).getText(), auditLog);
+	}
+
+	@Test(dependsOnMethods = "checkCustomerMaintenance")
+	public void checkOpenNotificationsPage() {
+		// step 7
+		action.clickBtn(By.id(CustomerMenu.NOTIFICATIONS));
+		action.waitObjVisible(By.cssSelector(CustomerMenu.DELETE_BUTTON));
+		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.HEADER_OF_PAGE)).getText(), notifications);
+	}
+
+	@Test(dependsOnMethods = "checkOpenNotificationsPage")
+	public void checkDocumentMenu() {
+		// step 8
+		action.clickBtn(By.id(CustomerMenu.DOCUMENTATION));
+		action.waitObjVisible(By.id(CustomerMenu.CONFIGURE_DOCUMENT));
+		assertEquals(driver.findElement(By.id(CustomerMenu.CONFIGURE_DOCUMENT)).getText(), configureDocument);
+		assertEquals(driver.findElement(By.id(CustomerMenu.USER_GUIDE)).getText(), userGuide);
+		assertEquals(driver.findElement(By.id(CustomerMenu.ABOUT)).getText(), about);
+
+	}
+
+	@Test(dependsOnMethods = "checkDocumentMenu")
+	public void checkPageOfCustomerMaintainance() {
+		action.clickBtn(By.id(CustomerMenu.DOCUMENTATION));
+		action.clickBtn(By.cssSelector(CustomerMenu.SUPPLIER_LIST));
+		action.waitObjVisible(By.cssSelector(CustomerMenu.DEACTIVE_ICON));
+		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.HEADER_OF_PAGE)).getText(), maintainSuppliers);
+		action.clickBtn(By.id(CustomerMenu.USER_GROUPS));
+		action.waitObjVisible(By.cssSelector(CustomerMenu.ADD_ICON));
+		action.pause(milliseconds);
+		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.HEADER_OF_PAGE)).getText(), maitainUserGroups);
+		action.clickBtn(By.id(CustomerMenu.USERS));
+		action.pause(milliseconds);
+		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.HEADER_OF_PAGE)).getText(), maintainCustomerUsers);
+		action.clickBtn(By.id(CustomerMenu.PROFILES));
+		action.waitObjVisible(By.cssSelector(CustomerMenu.MENU_ICON));
+		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.HEADER_OF_PAGE)).getText(), maintainCustomerDefinedProfiles);
+
+	}
+
+
+}
--- AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/CustomerMenu.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/CustomerMenu.java
@@ -9,5 +9,33 @@ public class CustomerMenu {
 
 	public static final String SUPPLIERLIST_SUBMENU = "#supplierMenu > span > span";
 	
+	// ------------------//
+	public static final String CUSTOMER_DASHBOARD = "menuItemCustomerAdmin";
+	public static final String CUSTOMER_DASHBOARD_WITHOUT_ICON = "#menuItemCustomerAdmin > span > span.valo-menu-item-caption";
+
+	public static final String CUSTOMER_MAINTENANCE = "custConfigParentButton";
+	public static final String CUSTOMER_MAINTENANCE_WITHOUT_ICON = "#custConfigParentButton > span > span.valo-menu-item-caption > div > span.v-menu-item-caption";
+	public static final String NOTIFICATIONS = "notificationButton";
+	public static final String NOTIFICATIONS_WITHOUT_ICON = "#notificationButton > span > span.valo-menu-item-caption";
+	public static final String DOCUMENTATION = "docTabParentButton";
+	public static final String DOCUMENTATION_WITHOUT_ICON = "#docTabParentButton > span > span.valo-menu-item-caption > div > span.v-menu-item-caption";
+	public static final String USER_GROUPS = "maintainCustomerUsergroupButton";
+	public static final String USERS = "maintainUserButton";
+	public static final String PROFLES = "maintainCustomerProfileButton";
+	public static final String DOCUMENT_FILTERS = "customerDefinedFilters";
+	public static final String BUSINESS_CODE_SET = "maintainCodeSetButton";
+	public static final String ADDRESS_CONTACT = "maintainContactButton";
+	public static final String CUSTOMISE_LITERALS = "maintainTranslatedText";
+	public static final String DOCUMENT_IN_ERROR = "documentErrorButton";
+	public static final String AUDIT_LOG = "auditLogButton";
+	public static final String DELETE_BUTTON = "#HeaderMenuBar > span:nth-child(3)";
+	public static final String HEADER_OF_PAGE = "#common-header-left-layout > div > div";
+	public static final String CONFIGURE_DOCUMENT = "configureDocumentationMenuItemButton";
+	public static final String USER_GUIDE = "userGuideMenuItemButton";
+	public static final String ABOUT = "aboutButton";
+	public static final String DEACTIVE_ICON = "#HeaderMenuBar > span:nth-child(4)";
+	public static final String ADD_ICON = "#HeaderMenuBar > span:nth-child(2)";
+	public static final String PROFILES = "maintainCustomerProfileButton";
+	public static final String MENU_ICON = "#HeaderMenuBar > span:nth-child(3)";
 	
 }
