1d0cc01e2bbd09aae114c607d75f92b9aa41a874
DonnaNGUYEN <donna@enclave.vn>
2017-11-15 17:30:53 +0700

    create TC 496

---
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/authencation/Supplier_Administrator_Dashboard.java
@@ -0,0 +1,118 @@
+package com.abb.ventyx.axis.authencation;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import com.abb.ventyx.axis.objects.pagedefinitions.AxisConfigMenu;
+import com.abb.ventyx.axis.objects.pagedefinitions.ScreenObjects;
+import com.abb.ventyx.axis.objects.pagedefinitions.SupplierMenu;
+import com.abb.ventyx.axis.objects.pagedefinitions.UserPreferences;
+import com.abb.ventyx.utilities.ALM;
+import com.abb.ventyx.utilities.BaseTestCase;
+import com.abb.ventyx.utilities.Credentials;
+import com.abb.ventyx.utilities.ScreenAction;
+import com.abb.ventyx.utilities.TableFunction;
+
+@ALM(id = "496")
+@Credentials(user = "supplier_user_1@abb.com", password = "Testuser1")
+public class Supplier_Administrator_Dashboard extends BaseTestCase {
+	ScreenAction action;
+	TableFunction table;
+	String supplierDashborad = "Supplier Dashboard";
+	int milliseconds = 3000;
+	String userName = "Administrator";
+	String userID = "supplier_user_1@abb.com";
+	String purchaseOrders = "Purchase Orders";
+	String shippingNotices = "Shipping Notices";
+	String invoices = "Invoices";
+	String administration = "Administration";
+	String documentation = "Documentation";
+	String userGroups = "User Groups";
+	String users = "Users";
+	String addressContact = "Address & Contact";
+	String auditLog = "Audit Log";
+	String businessCodeSets = "Business Code Sets";
+	String documentInError = "Documents in Error";
+	String maintainSupplierUserGroups = "Maintain Supplier User Groups";
+	String maintainSupplierUsers = "Maintain Supplier Users";
+	@Test
+	public void openScreen() {
+		// Step 1
+		action = new ScreenAction(driver);
+		action.assertTitleScreen(supplierDashborad);
+	}
+	@Test(dependsOnMethods = "openScreen")
+	public void checkTopRibbon() {
+		// step 2
+		assertEquals(driver.findElement(By.id(AxisConfigMenu.LANGUAGE)).getText(), "EN");
+		assertEquals(driver.findElement(By.id(UserPreferences.PROFILE_PANEL)).getText(), userName);
+		action.clickBtn(By.id(UserPreferences.PROFILE_PANEL));
+		action.waitObjVisible(By.id(ScreenObjects.SIGNOUT_BUTTON));
+		assertEquals(driver.findElement(By.cssSelector(AxisConfigMenu.USER_LOGIN)).getText(), userName);
+		assertEquals(driver.findElement(By.cssSelector(AxisConfigMenu.USER_ID)).getText(), userID);
+		action.clickBtn(By.id(UserPreferences.PROFILE_PANEL));
+	}
+
+	@Test(dependsOnMethods = "checkTopRibbon")
+	public void checkMenuOptions() {
+		// Step 3
+		assertEquals(driver.findElement(By.cssSelector(SupplierMenu.SUPPLIER_DASHBOARD_WITHOUT_ICON)).getText(), supplierDashborad);
+		assertEquals(driver.findElement(By.cssSelector(SupplierMenu.PURCHASE_ORDERS_WITHOUT_ICON)).getText(), purchaseOrders);
+		assertEquals(driver.findElement(By.cssSelector(SupplierMenu.SHIPPING_NOTICES_WITHOUT_ICON)).getText(), shippingNotices);
+		assertEquals(driver.findElement(By.cssSelector(SupplierMenu.INVOICES_WITHOUT_ICON)).getText(), invoices);
+		assertEquals(driver.findElement(By.cssSelector(SupplierMenu.ADMINISTRATION_WITHOUT_ICON)).getText(), administration);
+		assertEquals(driver.findElement(By.cssSelector(SupplierMenu.DOCUMENTATION_WITHOUT_ICON)).getText(), documentation);
+	}
+
+	@Test(dependsOnMethods = "checkMenuOptions")
+	public void CheckPageOfSupplierDashBoard() {
+		// Step 4,5,6
+		action.clickBtn(By.id(SupplierMenu.PURCHASE_ORDERS_ID));
+		action.waitObjVisible(By.cssSelector(SupplierMenu.HEADER_OF_PAGE));
+		action.pause(milliseconds);
+		assertEquals(driver.findElement(By.cssSelector(SupplierMenu.HEADER_OF_PAGE)).getText(), purchaseOrders);
+		action.clickBtn(By.id(SupplierMenu.SHIPPING_NOTICES_ID));
+		action.waitObjVisible(By.cssSelector(SupplierMenu.HEADER_OF_PAGE));
+		action.pause(milliseconds);
+		assertEquals(driver.findElement(By.cssSelector(SupplierMenu.HEADER_OF_PAGE)).getText(), shippingNotices);
+		action.clickBtn(By.id(SupplierMenu.INVOICES_ID));
+		action.waitObjVisible(By.cssSelector(SupplierMenu.HEADER_OF_PAGE));
+		action.pause(milliseconds);
+		assertEquals(driver.findElement(By.cssSelector(SupplierMenu.HEADER_OF_PAGE)).getText(), invoices);
+	}
+
+	@Test(dependsOnMethods = "CheckPageOfSupplierDashBoard")
+	public void checkSubmenuOfAdministration() {
+		// Step 7
+		action.clickBtn(By.id(SupplierMenu.ADMINISTRATION_ID));
+		action.waitObjVisible(By.id(SupplierMenu.USER_GROUPS_ID));
+		assertEquals(driver.findElement(By.id(SupplierMenu.USER_GROUPS_ID)).getText(), userGroups);
+		assertEquals(driver.findElement(By.id(SupplierMenu.USERS_ID)).getText(), users);
+		assertEquals(driver.findElement(By.id(SupplierMenu.ADDRESS_CONTACT_ID)).getText(), addressContact);
+		assertEquals(driver.findElement(By.id(SupplierMenu.AUDIT_LOG_ID)).getText(), auditLog);
+		assertEquals(driver.findElement(By.id(SupplierMenu.BUSINESS_CODE_SETS_ID)).getText(), businessCodeSets);
+		assertEquals(driver.findElement(By.id(SupplierMenu.DOCUMENT_IN_ERROR_ID)).getText(), documentInError);
+	}
+
+	@Test(dependsOnMethods = "checkSubmenuOfAdministration")
+	public void checkPageOfAdministration() {
+		// Step 8
+		action.clickBtn(By.id(SupplierMenu.USER_GROUPS_ID));
+		action.waitObjVisible(By.cssSelector(SupplierMenu.HEADER_OF_PAGE));
+		action.pause(milliseconds);
+		assertEquals(driver.findElement(By.cssSelector(SupplierMenu.HEADER_OF_PAGE)).getText(), maintainSupplierUserGroups);
+
+		action.clickBtn(By.id(SupplierMenu.USERS_ID));
+		action.waitObjVisible(By.cssSelector(SupplierMenu.HEADER_OF_PAGE));
+		action.pause(milliseconds);
+		assertEquals(driver.findElement(By.cssSelector(SupplierMenu.HEADER_OF_PAGE)).getText(), maintainSupplierUsers);
+
+
+	}
+
+
+
+
+}
--- AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/SupplierMenu.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/SupplierMenu.java
@@ -3,10 +3,15 @@ package com.abb.ventyx.axis.objects.pagedefinitions;
 public class SupplierMenu {
 
 	public static final String SUPPLIER_DASHBOARD_ID = "menuItemSupplier";
+	public static final String SUPPLIER_DASHBOARD_WITHOUT_ICON = "#menuItemSupplier > span > span.valo-menu-item-caption";
 	public static final String PURCHASE_ORDERS_ID = "menuItemPurchaseOrder";
+	public static final String PURCHASE_ORDERS_WITHOUT_ICON = "#menuItemPurchaseOrder > span > span.valo-menu-item-caption";
 	public static final String INVOICES_ID = "menuItemInvoice";
+	public static final String INVOICES_WITHOUT_ICON = "#menuItemInvoice > span > span.valo-menu-item-caption";
 	public static final String SHIPPING_NOTICES_ID = "menuItemAsn";
+	public static final String SHIPPING_NOTICES_WITHOUT_ICON = "#menuItemAsn > span > span.valo-menu-item-caption";
 	public static final String ADMINISTRATION_ID = "suppAdminParentButton";
+	public static final String ADMINISTRATION_WITHOUT_ICON = "#suppAdminParentButton > span > span.valo-menu-item-caption > div > span.v-menu-item-caption";
 	public static final String USER_GROUPS_ID = "maintainSupplierUsergroupButton";
 	public static final String USERS_ID = "maintainUserButton";
 	public static final String ADDRESS_CONTACT_ID = "maintainContactButton";
@@ -14,8 +19,11 @@ public class SupplierMenu {
 	public static final String BUSINESS_CODE_SETS_ID = "menuItemSupplierCodeSet";
 	public static final String DOCUMENT_IN_ERROR_ID = "documentErrorButton";
 	public static final String DOCUMENTATION_ID = "docTabParentButton";
+	public static final String DOCUMENTATION_WITHOUT_ICON = "#docTabParentButton > span > span.valo-menu-item-caption > div > span.v-menu-item-caption";
 	public static final String USER_GUIDE_ID = "userGuideMenuItemButton";
 	public static final String ABOUT_ID = "aboutButton";
 	public static final String SUPPLIER_NAME = "companyNameField";
+	// -----------------------------------//
+	public static final String HEADER_OF_PAGE = "#common-header-left-layout > div > div";
 
 }
