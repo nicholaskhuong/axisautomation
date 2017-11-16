d775102e4d814cc0c3f49013310b47e34be7278c
Perla <perla@enclave.vn>
2017-11-16 08:16:58 +0700

    Increated waittime

--- AxisP2PAutomation/src/com/abb/ventyx/axis/support/Profile_Create.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/support/Profile_Create.java
@@ -73,7 +73,7 @@ public class Profile_Create extends BaseTestCase {
 	@Test(dependsOnMethods = "inputProfileNameandCustomerName", alwaysRun = true)
 	public void slelectAuthorisedDocumentTypes() {
 		action.clickCheckBoxN(4);
-		action.pause(milliseconds);
+		action.pause(2000);
 		action.waitObjVisible(By.id(Profiles.SAVE_BTN));
 		action.waitObjVisibleAndClick(By.id(Profiles.SAVE_BTN));
 		action.pause(milliseconds);
