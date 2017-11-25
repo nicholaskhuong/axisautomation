bd24bea78254221b77f3029be1b1f4de1cc45248
Phuong (Perla) T. TRINH <perla@enclave.vn>
2017-11-25 17:47:34 +0700

    Update xpath

--- AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/DocumentsInError.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/objects/pagedefinitions/DocumentsInError.java
@@ -2,5 +2,5 @@ package com.abb.ventyx.axis.objects.pagedefinitions;
 
 public interface DocumentsInError {
 
-	public static final String DOC_TYPE_FILTER_XPATH = "(//input[@id='filterField'])[3]";
+	public static final String DOC_TYPE_FILTER_XPATH = "(//input[@id='filterField'])[2]";
 }
