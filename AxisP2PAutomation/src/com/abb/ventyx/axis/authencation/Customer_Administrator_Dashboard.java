b5abbee5257b7f85cf3c7f4159e4cf4f01e34c77
DonnaNGUYEN <donna@enclave.vn>
2017-11-29 15:15:50 +0700

    fixd some faild

--- AxisP2PAutomation/src/com/abb/ventyx/axis/authencation/Customer_Administrator_Dashboard.java
+++ AxisP2PAutomation/src/com/abb/ventyx/axis/authencation/Customer_Administrator_Dashboard.java
@@ -133,10 +133,11 @@ public class Customer_Administrator_Dashboard extends BaseTestCase {
 		action.pause(milliseconds);
 		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.HEADER_OF_PAGE)).getText(), maintainCustomerDefinedProfiles);
 		action.clickBtn(By.id(CustomerMenu.DOCUMENT_FILTERS));
-		action.waitObjVisible(By.cssSelector(CustomerMenu.DOCUMENT_FILTER_HEADER));
+		action.waitObjVisible(By.cssSelector(CustomerMenu.BUSINESS_CODE_SETS_HEADER));
 		action.pause(milliseconds);
 		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.HEADER_OF_PAGE)).getText(), customerDefinedFilters);
 		action.clickBtn(By.id(CustomerMenu.BUSINESS_CODE_SET));
+
 		action.waitObjVisible(By.cssSelector(CustomerMenu.BUSINESS_CODE_SETS_HEADER));
 		action.pause(milliseconds);
 		assertEquals(driver.findElement(By.cssSelector(CustomerMenu.HEADER_OF_PAGE)).getText(), maintainBusinessCodeSets);
